b716480c7f598be139ab12f1f01ab5f2d9849f9e
ADMIN <karenhoya667@gmail.com>
2024-09-12 17:30:02 -0500

    Codigo funcionando

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//Karen Audrey Hoya Salinas
+//Clase de Vehiculo
+//12-09.2024
+
+public class Main {
+    public static void main(String[] args) {
+        List<Vehiculo>listaVehiculos=new ArrayList<>();
+        Scanner scanner=new Scanner(System.in);
+        while (true){
+            System.out.println("Menu");
+            System.out.println("1.Agregar");
+            System.out.println("2.Mostrar");
+            System.out.println("3.Salir");
+            int opcion= scanner.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Ingrese la placa: ");
+                    String placa = scanner.next();
+
+                    System.out.println("Ingrese la marca: ");
+                    String marca = scanner.next();
+
+                    System.out.println("Ingrese el modelo: ");
+                    String modelo = scanner.next();
+
+                    System.out.println("Ingrese el tipo: ");
+                    String tipo = scanner.next();
+
+                    System.out.println("Ingrese el color: ");
+                    String color = scanner.next();
+
+                    listaVehiculos.add(new Vehiculo(placa, marca, modelo, tipo, color));
+
+                    break;
+                case 2:
+                    for (Vehiculo vehiculo:listaVehiculos){
+                        System.out.println("--------------------------");
+                        System.out.println("LOS DATOS DEL VEHICULO SON:");
+                        System.out.println("Placa: "+vehiculo.getPlaca());
+                        System.out.println("Marca: "+vehiculo.getMarca());
+                        System.out.println("Modelo: "+vehiculo.getModelo());
+                        System.out.println("Tipo: "+vehiculo.getTipo());
+                        System.out.println("Color: "+vehiculo.getColor());
+
+                    }
+                    System.out.println("-------------------------");
+                    break;
+                case 3:
+                    System.out.println("Saliendo.....");
+                    return;
+                default:
+                    System.out.println("Opcion Invalida");
+            }
+        }
+    }
+}
